2a38499767a7394d1aff2371f45316b411805520
Melina <melina.llacua@gmail.com>
2023-05-18 16:14:36 -0500

    lector de IMEI

--- app/src/main/java/com/example/nfcetiqueta/ObtenerIMEI.java
+++ app/src/main/java/com/example/nfcetiqueta/ObtenerIMEI.java
@@ -5,13 +5,18 @@ import android.content.SharedPreferences;
 import android.os.Build;
 import android.provider.Settings;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class ObtenerIMEI {
 
     public static String getDeviceId(Context context) {
-        // Verificar si ya se ha generado un identificador único previamente
-        SharedPreferences preferences = context.getSharedPreferences("MyAppPreferences", Context.MODE_PRIVATE);
-        String deviceId = preferences.getString("device_id", null);
+        String deviceId = readDeviceIdFromFile(context);
 
         if (deviceId == null) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -25,12 +30,59 @@ public class ObtenerIMEI {
                 }
             }
 
-            // Guardar el identificador en las preferencias compartidas
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putString("device_id", deviceId);
-            editor.apply();
+            if (deviceId != null) {
+                saveDeviceIdToFile(context, deviceId);
+            }
         }
 
         return deviceId;
+    }
+
+    private static String readDeviceIdFromFile(Context context) {
+        String deviceId = null;
+        FileInputStream fis = null;
+        BufferedReader reader = null;
+
+        try {
+            File file = new File(context.getExternalFilesDir(null), "device_id.txt");
+            fis = new FileInputStream(file);
+            reader = new BufferedReader(new InputStreamReader(fis));
+            deviceId = reader.readLine();
+        } catch (Exception e) {
+            Log.e("ObtenerIMEI", "Error al leer el archivo de identificación del dispositivo", e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (Exception e) {
+                Log.e("ObtenerIMEI", "Error al cerrar los flujos de lectura del archivo", e);
+            }
+        }
+
+        return deviceId;
+    }
+
+    private static void saveDeviceIdToFile(Context context, String deviceId) {
+        FileOutputStream fos = null;
+
+        try {
+            File file = new File(context.getExternalFilesDir(null), "device_id.txt");
+            fos = new FileOutputStream(file);
+            fos.write(deviceId.getBytes());
+        } catch (Exception e) {
+            Log.e("ObtenerIMEI", "Error al guardar el identificador del dispositivo en el archivo", e);
+        } finally {
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (Exception e) {
+                Log.e("ObtenerIMEI", "Error al cerrar el flujo de escritura del archivo", e);
+            }
+        }
     }
 }
